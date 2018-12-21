b7aaedd0d8216a8f3662011625fe7c28878c175b
kmelnikov98 <33918043+kmelnikov98@users.noreply.github.com>
2018-12-20 16:14:48 -0800

    banking user and pass
    created the username and password description

--- Project_finance/Program.cs
+++ Project_finance/Program.cs
@@ -4,22 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project_finance
+
+namespace UserAndPass
 {
+
+    class User
+    {
+        string username;
+        string password;
+
+
+        public string GetUsername()
+        {
+            return username;
+        }
+
+        public void SetName()
+        {
+            Console.WriteLine("Enter a username: ");
+            username = Console.ReadLine();
+        }
+
+        public void SetPassword()
+        {
+            Console.WriteLine("Select a password: ");
+            password = Console.ReadLine();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            bool end = false;
-            while (!end)
-            {
-
-                Console.WriteLine("Type anything to terminate program: ");
-                int c = Console.Read();
-                end = true;
-            }
+            User one = new User();
+            one.SetName();
+            one.SetPassword();
+            Console.WriteLine("Username Chosen: {0}", one.GetUsername());
         }
     }
 }
